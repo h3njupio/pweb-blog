e24d36f34da490771f05652f92daaaaf5ec4354d
h3njupio <h3njupio@gmail.com>
2018-02-28 17:02:54 +0900

    add new misc app

--- misc/views.py
+++ misc/views.py
@@ -8,8 +8,16 @@ def misc_list(request):
     if request.method == 'POST':
         user_id = request.POST['user_id']
         user_pw = request.POST['user_pw']
-        driver = m_chrome_driver()
-        wfms(driver, user_id, user_pw)
+        platform_type = request.POST['platform_type']
+        print(platform_type)
+        result = ""
+        if platform_type == "PC":
+            driver = chrome_driver()
+            result = wfms(driver, user_id, user_pw)
+        elif platform_type == "MOBILE":
+            result = m_chrome_driver(user_id, user_pw)
+
+        return render(request, 'misc/misc_list.html', {'status':result})
 
     elif request.method == 'GET':
         return render(request, 'misc/misc_list.html')
@@ -29,15 +37,30 @@ def chrome_driver():
     return chrome_driver
 
 
-def m_chrome_driver():
+def m_chrome_driver(user_id, user_pw):
     desired_caps = {}
-    desired_caps['platformName'] = 'Android'
-    desired_caps['platformVersion'] = '4.2'
-    desired_caps['deviceName'] = 'Android Emulator'
-    desired_caps['app'] = '../../../apps/selendroid-test-app.apk'
+    desired_caps['testdroid_username'] = 'ville - veikko.helppi @ bitbar.com'
+    desired_caps['testdroid_password'] = 'xxxxxxxx'
+    desired_caps['testdroid_target'] = 'chrome'
+    desired_caps['testdroid_project'] = 'Appium Chrome'
+    desired_caps['testdroid_testrun'] = 'TestRun 1'
+    desired_caps['testdroid_device'] = 'Asus Google Nexus 7 (2013) ME571KL'
+    desired_caps['platformName'] = 'android'
+    desired_caps['deviceName'] = 'AndroidDevice'
+    desired_caps['browserName'] = 'chrome'
 
-    m_chrome_driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-    return m_chrome_driver
+    m_chrome_driver = mobiledriver.Remote('https://infra.bnksys.co.kr', desired_caps)
+    m_chrome_driver.driver.find_element_by_id('text_id').send_keys(user_id)
+    m_chrome_driver.find_element_by_id('text_password').send_keys(user_pw)
+    m_chrome_driver.execute_script("document.getElementById('externalIP').value='210.183.119.193'")
+    m_chrome_driver.find_element_by_class_name('login_btn').click()
+
+    html = m_chrome_driver.page_source
+    soup = BeautifulSoup(html, 'lxml')
+    location = soup.select('div.location > h1')
+    m_chrome_driver.quit()
+
+    return location[0].text
 
 
 def wfms(driver, user_id, user_pw):
@@ -54,5 +77,6 @@ def wfms(driver, user_id, user_pw):
     html = driver.page_source
     soup = BeautifulSoup(html, 'lxml')
     location = soup.select('div.location > h1')
+    driver.quit()
 
     return location[0].text
